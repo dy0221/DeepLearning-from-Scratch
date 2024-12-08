7f7598b7c8ae33fd8e24c60b912cbac1ee7be9a3
dy0221 <wjm2517@hanyang.ac.kr>
2024-12-08 23:35:57 +0900

    study chap 4, 5

--- Neueral_network_learning/cross_entropy_error.py
+++ Neueral_network_learning/cross_entropy_error.py
@@ -1,8 +1,19 @@
 import numpy as np
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    # if t.dtype != int:  # 정수형으로 변환
+    #     t = t.astype(int)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size # t가 float64여서 에러 발생 << int 형으로
 
 if __name__=='__main__':
     t = [0, 0 , 1, 0 , 0, 0, 0, 0, 0, 0]
