852f469e65dc6bc2a73fcf393da9af82bdb4de09
dy0221 <wjm2517@hanyang.ac.kr>
2025-01-04 02:27:19 +0900

    add comment

--- Learning_skill/batch_normalization.py
+++ Learning_skill/batch_normalization.py
@@ -32,28 +32,39 @@ class BatchNormalization:
         return out.reshape(*self.input_shape)
             
     def __forward(self, x, train_flg):
-        if self.running_mean is None:
+        # 처음 시작할때, 차원을 맞춰서 0으로 초기화
+        if self.running_mean is None: 
             N, D = x.shape
             self.running_mean = np.zeros(D)
             self.running_var = np.zeros(D)
-                        
+        # 학습중이라면                
         if train_flg:
+            # 평균 구하기
             mu = x.mean(axis=0)
+            # 분산 구하기 
+            # 입력값 - 평균값 
             xc = x - mu
+            # (입력값 - 평균값)의 제곱의 평균
             var = np.mean(xc**2, axis=0)
+            # 분산의 제곱의 제곱 근
             std = np.sqrt(var + 10e-7)
+            # 정규화된 값
             xn = xc / std
             
             self.batch_size = x.shape[0]
             self.xc = xc
             self.xn = xn
             self.std = std
+            # 평균과 분산을 지수 이동 평균으로 저장
             self.running_mean = self.momentum * self.running_mean + (1-self.momentum) * mu
             self.running_var = self.momentum * self.running_var + (1-self.momentum) * var            
         else:
+            # 학습이 아닌 경우(추론시)>> 학습된 running_mean과 running_var로 정규화
+            # 입력값 - 평균값 
             xc = x - self.running_mean
+             # 분산의 제곱의 제곱 근
             xn = xc / ((np.sqrt(self.running_var + 10e-7)))
-            
+        # 결과값    
         out = self.gamma * xn + self.beta 
         return out
 
